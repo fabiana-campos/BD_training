2e648dd152426edde966291fefabd86f6f8fd5fb
Fabiana Campos Serra dos Santos <20231014040033@ifrn.local>
2024-11-13 11:38:52 -0300

    Aula 2: Configuração do Django_Rest com Postgres e Swagger

--- amazon/backend/models.py
+++ amazon/backend/models.py
@@ -10,3 +10,16 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nome
+        
+
+class Endereco(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    rua = models.CharField(max_length=100)
+    numero = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    estado = models.CharField(max_length=2) 
+    cep = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.rua}, {self.numero} - {self.bairro}, {self.cidade}/{self.estado} - {self.cep}'
